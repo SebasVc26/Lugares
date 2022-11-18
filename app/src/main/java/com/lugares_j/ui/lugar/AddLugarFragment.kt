096d3373d0d5df5a47bbf186e763528d599649e7
SebasVc26 <sebas2000_vc@hotmail.com>
2022-11-17 20:23:21 -0600

    Semana 10

--- app/src/main/java/com/lugares_j/ui/lugar/AddLugarFragment.kt
+++ app/src/main/java/com/lugares_j/ui/lugar/AddLugarFragment.kt
@@ -1,22 +1,34 @@
 package com.lugares_j.ui.lugar
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 import com.lugares_j.R
 import com.lugares_j.databinding.FragmentAddLugarBinding
-import com.lugares_j.databinding.FragmentLugarBinding
 import com.lugares_j.model.Lugar
+import com.lugares_j.utiles.AudioUtiles
+import com.lugares_j.utiles.ImagenUtiles
 import com.lugares_j.viewmodel.LugarViewModel
 
 class AddLugarFragment : Fragment() {
@@ -27,6 +39,10 @@ class AddLugarFragment : Fragment() {
     private var _binding: FragmentAddLugarBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var audioUtiles: AudioUtiles
+    private lateinit var imagenUtiles: ImagenUtiles
+    private lateinit var tomarFotoActivity: ActivityResultLauncher<Intent>
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,14 +50,110 @@ class AddLugarFragment : Fragment() {
     ): View {
          lugarViewModel =
             ViewModelProvider(this).get(LugarViewModel::class.java)
-
         _binding = FragmentAddLugarBinding.inflate(inflater, container, false)
-
-        binding.btAdd.setOnClickListener { addLugar() }
+        binding.btAdd.setOnClickListener {
+            binding.progressBar.visibility = ProgressBar.VISIBLE
+            binding.msgMensaje.text = getString(R.string.msg_subiendo_audio)
+            binding.msgMensaje.visibility= TextView.VISIBLE
+            subeNota()
+        }
 
         activaGPS()
 
+        audioUtiles = AudioUtiles(
+            requireActivity(),
+            requireContext(),
+            binding.btAccion,
+            binding.btPlay,
+            binding.btDelete,
+            getString(R.string.msg_graba_audio),
+            getString(R.string.msg_detener_audio))
+
+        tomarFotoActivity = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult())   {
+            if(it.resultCode == Activity.RESULT_OK){
+                imagenUtiles.actualizaFoto()
+            }
+        }
+
+        imagenUtiles = ImagenUtiles(
+            requireContext(),
+            binding.btPhoto,
+            binding.btRotaL,
+            binding.btRotaR,
+            binding.imagen,
+            tomarFotoActivity)
+
         return binding.root
+    }
+
+    //Esta funcion sube la nota de audio al storage y pasa la ruta publiica del archivo a la siguiente funcion
+    private fun subeNota() {
+        val archivoLocal = audioUtiles.audioFile
+        if(archivoLocal.exists()
+            && archivoLocal.isFile &&
+                archivoLocal.canRead()){
+            //Se fija la ruta (uri) del achivoLocal de audio
+            val rutaLocal = Uri.fromFile(archivoLocal)
+
+            //Se establece la ruta del archivo en la nota de audio
+            val rutaNube =
+                "lugaresApp/${Firebase.auth.currentUser?.email}/audios/${archivoLocal.name}"
+            // ejemplo "lugaresApp/juan33@gmail.com/audios/20221117190005.mp3"
+            //Se hace la referencia Real...
+            val referencia: StorageReference = Firebase.storage.reference.child(rutaNube)
+
+            //Se sube el archivo y se establece lo "listen" para saber que hacer...
+            referencia.putFile(rutaLocal)
+                .addOnSuccessListener {
+                    referencia.downloadUrl
+                        .addOnSuccessListener {
+                           //Se obtiene la ruta publica del archivo
+                            val rutaAudio = it.toString()
+                            subeImagen(rutaAudio)
+                        }
+                }
+                .addOnFailureListener{
+                    subeImagen("")
+                }
+        } else { //No hay foto o hay un error en el archivo o algo que no puede leer
+            subeImagen("")
+        }
+    }
+
+    private fun subeImagen(rutaAudio: String) {
+        binding.msgMensaje.text = getString(R.string.msg_subiendo_imagen)
+
+        val archivoLocal = imagenUtiles.imagenFile
+        if(archivoLocal.exists() &&
+            archivoLocal.isFile &&
+            archivoLocal.canRead()){
+            //Se fija la ruta (uri) del achivoLocal de foto
+            val rutaLocal = Uri.fromFile(archivoLocal)
+
+            //Se establece la ruta del archivo en la nota de foto
+            val rutaNube =
+                "lugaresApp/${Firebase.auth.currentUser?.email}/imagenes/${archivoLocal.name}"
+            // ejemplo "lugaresApp/juan33@gmail.com/imagenes/20221117190005.jpg"
+            //Se hace la referencia Real...
+            val referencia: StorageReference = Firebase.storage.reference.child(rutaNube)
+
+            //Se sube el archivo y se establece lo "listen" para saber que hacer...
+            referencia.putFile(rutaLocal)
+                .addOnSuccessListener {
+                    referencia.downloadUrl
+                        .addOnSuccessListener {
+                            //Se obtiene la ruta publica del archivo
+                            val rutaImagen = it.toString()
+                            addLugar(rutaAudio, rutaImagen)
+                        }
+                }
+                .addOnFailureListener{
+                    addLugar(rutaAudio, "")
+                }
+        } else { //No hay foto o hay un error en el archivo o algo que no puede leer
+            addLugar(rutaAudio, "")
+        }
     }
 
     private fun activaGPS(){
@@ -75,7 +187,8 @@ class AddLugarFragment : Fragment() {
         }
     }
 
-    private fun addLugar(){
+    private fun addLugar(rutaAudio: String, rutaImagen: String) {
+        binding.msgMensaje.text = getString(R.string.msg_subiendo_lugar)
         val nombre=binding.etNombre.text.toString() //obtiene el texto de lo que el usuario escribio
         if (nombre.isNotEmpty()){//Si escribio algo en el nombre, se puede guardar el lugar
             val correo=binding.etCorreo.text.toString() //obtiene el texto de lo que el usuario escribio
@@ -85,7 +198,7 @@ class AddLugarFragment : Fragment() {
             val longitud = binding.tvLongitud.text.toString().toDouble()
             val altura = binding.tvAltura.text.toString().toDouble()
             val lugar = Lugar("",nombre,correo,telefono,web,
-                latitud,longitud,altura,"","")
+                latitud,longitud,altura,rutaAudio,rutaImagen)
 
 
             //Se procede a registrar el nuevo lugar
