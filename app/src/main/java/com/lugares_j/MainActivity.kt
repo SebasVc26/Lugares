9f1879ef23a9750565e7880edcc71216a82183e7
SebasVc26 <sebas2000_vc@hotmail.com>
2022-11-30 00:09:48 -0600

    Semana 11

--- app/src/main/java/com/lugares_j/MainActivity.kt
+++ app/src/main/java/com/lugares_j/MainActivity.kt
@@ -4,9 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.lugares_j.databinding.ActivityMainBinding
@@ -16,6 +22,9 @@ class MainActivity : AppCompatActivity() {
     //Definicion del objeto para hacer la autenticacion
     private lateinit var auth : FirebaseAuth
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +40,48 @@ class MainActivity : AppCompatActivity() {
 
         binding.btRegister.setOnClickListener { haceRegistro() }
         binding.btLogin.setOnClickListener { haceLogin() }
+
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+        binding.btGoogle.setOnClickListener {googleSignIn()}
+    }
+
+    private fun googleSignIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent,5000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 5000){
+            val tarea = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val cuenta = tarea.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(cuenta.idToken)
+            } catch (e: ApiException){
+                
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credenciales = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credenciales)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful){//Si pudo crear el usuario
+                    Log.d("Autenticando", "usuario autenticado")
+                    val user = auth.currentUser //Recupero la info del usuario creado
+                    Actualiza(user)
+                } else{
+                    Log.d("Autenticando", "Error autenticando usuario")
+                }
+                Actualiza(null)
+            }
     }
 
     private fun haceRegistro() {
@@ -45,10 +96,11 @@ class MainActivity : AppCompatActivity() {
                 if (task.isSuccessful){//Si pudo crear el usuario
                     Log.d("Autenticando", "usuario creado")
                      user = auth.currentUser //Recupero la info del usuario creado
+                     Actualiza(user)
                 } else{
                     Log.d("Autenticando", "Error creando usuario")
                 }
-                Actualiza(user)
+                Actualiza(null)
             }
     }
 
